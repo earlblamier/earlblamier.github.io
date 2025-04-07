36bbdc94c943a1ec7c4c778ddfb92b61eb7fd955
Earl Lamier <77307962+earlblamier@users.noreply.github.com>
2025-04-07 13:37:23 -0400

    Deploy Angular 19 clear local docs and copy safely

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component'; // Your existing component
+import { AboutComponent } from './about/about.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
-  { path: '', component: MainContentComponent, pathMatch: 'full' }, // Default route with `pathMatch: 'full'`
+  { path: '', component: MainContentComponent, pathMatch: 'full' }, // Default route
+  { path: 'about', component: AboutComponent },
+  { path: 'stack', component: TechStackComponent },
+  { path: 'projects', component: CardComponent },
+  { path: 'contact', component: FooterComponent },
   { path: '**', redirectTo: '' } // Wildcard route for undefined paths
 ];
