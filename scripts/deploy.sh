7b538f9f43af61b843d7e2d6647179d96a91edde
Earl Lamier <77307962+earlblamier@users.noreply.github.com>
2025-04-09 23:52:14 -0400

    Deploy Angular 19 clear local docs and copy safely

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,25 @@
 # earl-lamier to https://earlblamier.github.io/
 # Build the Angular application
 # ng build --configuration production
-rm -rf docs/* # Remove old files in the docs folder
+
+# Enable extended globbing
+shopt -s extglob
+
+# Remove all files and folders in the docs directory except CNAME
+rm -rf docs/!(CNAME)
 
 ng build
 
 # Copy the build output to the docs folder
 cp -r docs/browser/* docs/
+
+# Check if CNAME file exists, if not, create it and write "earl-lamier.dev"
+if [ ! -f docs/CNAME ]; then
+  echo "earl-lamier.dev" > docs/CNAME
+  echo "CNAME file created with 'earl-lamier.dev'"
+else
+  echo "CNAME file already exists"
+fi
 
 # Git commit and push changes
 git add .
